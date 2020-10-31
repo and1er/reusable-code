f40ad182ffc47ff593f13832428c8b38eae3b7b1
and1er <34434784+and1er@users.noreply.github.com>
2020-10-31 22:11:23 +0300

    `bash/get_dir_items_without_ls.sh` was added.

---
+++ bash/get_dir_items_without_ls.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo === VARIOUS WAYS TO GET DIR\' FILES AND SUBFOLDERS WITHOUT ls COMMAND ===
+
+echo == find ==
+find . -mindepth 1
+
+echo == tree ==
+tree
+
+echo == 'echo' ==
+echo *
